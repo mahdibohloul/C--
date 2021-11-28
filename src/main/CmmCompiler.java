e6c57eee7b25c890d0378ab7a97d6e26d9b34b8e
Mahdi Bohloul <mehdi.bohlol@tapsi.net>
2021-11-28 20:10:45 +0330

    Prepare project for phase2

---
+++ src/main/CmmCompiler.java
@@ -0,0 +1,19 @@
+package main;
+
+import main.ast.nodes.Program;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import parsers.CmmLexer;
+import parsers.CmmParser;
+
+public class CmmCompiler {
+    public void compile(CharStream textStream) {
+        CmmLexer cmmLexer = new CmmLexer(textStream);
+        CommonTokenStream tokenStream = new CommonTokenStream(cmmLexer);
+        CmmParser cmmParser = new CmmParser(tokenStream);
+
+        Program program = cmmParser.cmm().cmmProgram;
+
+        //todo
+    }
+}
